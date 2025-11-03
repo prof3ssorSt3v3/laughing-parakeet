f950e09f142c9459e7f897dcb9e6e1d1b25ae807
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2025-11-03 09:44:21 -0500

    starter code week 10

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*
+URL
+URLSearchParams
+Request
+Response
+*/
+
+document.addEventListener('DOMContentLoaded', init);
+
+const BASEURL = 'https://jsonplaceholder.typicode.com/';
+
+function init() {
+  //when the html has been read
+}
+
+function getData() {
+  //retrieve an array of data from the API
+}
